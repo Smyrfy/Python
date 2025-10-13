4c08e544ed7d1123f4289a2b6dbd94d7822d147b
Smyrfy <dudko.dasha72773@gmail.com>
2025-10-14 00:12:32 +0300

    Update Mouse Events.py

--- Mouse Events.py
+++ Mouse Events.py
@@ -9,23 +9,36 @@ pygame.display.set_caption ("Mouse Event")
 
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 FPS = 60
 clock = pygame.time.Clock()
 
+sc.fill(WHITE)
+pygame.display.update()
+sp = None
+
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:FPS = 60
+        if event.type == pygame.QUIT:
+            exit()
+    sc.fill(WHITE)
+    pos = pygame.mouse.get_pos()
+    if pygame.mouse.get_focused():
+        pygame.draw.circle(sc, BLUE, pos, 7)
 
-        clock = pygame.time.Clock()
-        while 1:
-           for event in pygame.event.get():
-               if event.type == pygame.QUIT:
-                   exit()
+    pygame.display.update()
 
-               elif event.type == pygame.MOUSEBUTTONDOWN:
-                   print("Нажата кнопка: ", event.button)
-               elif event.type == pygame.MOUSEMOTION:
-                   print("Позиция мыши: ", event.pos)
-           clock.tick(FPS)
+    pressed = pygame.mouse.get_pressed()
+    if pressed[0]:
+        if sp is None:
+            sp = pygame.mouse.get_pos()
+        pos = pygame.mouse.get_pos()
+        width = pos[0] - sp[0]
+        height = pos[1] - sp[1]
+        sc.fill(WHITE)
+        pygame.draw.rect(sc, RED, (sp[0], sp[1], width, height), 2)
+        pygame.display.update()
+    else:
+        sp = None
+
+    clock.tick(FPS)
